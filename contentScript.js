33344c840d627542cfb3caf49c36ed05bd3f9e81
vankobe <egami2787@gmail.com>
2013-12-15 20:05:34 +0900

    delete comments and change order

--- contentScript.js
+++ contentScript.js
@@ -1,32 +1,11 @@
-/*
-画面ロード時にpopupを初期化する場合 => 最後にロードした画面のseoタグが見れる
-$(function(){
-	console.log("get_content")
-	seo_object = {};
-	seo_object = {
-		h1 : $("h1").text(),
-		title : $("title").text(),
-		description : $("meta[name=description]").attr("content"),
-		keywords : $("meta[name=keywords]").attr("content"),
-		url : document.URL,
-		canonical : $("link[rel=canonical]").attr("href"),
-		og_type : $('meta[property="og:type"]').attr("content"),
-		og_site_name : $('meta[property="og:site_name"]').attr("content"),
-		og_title : $('meta[property="og:title"]').attr("content"),
-		og_image : $('meta[property="og:image"]').attr("content"),
-		og_description : $('meta[property="og:description"]').attr("content")
-	};
-	seo_object.action = "get_seo_object";
-	chrome.runtime.sendMessage(seo_object)	
-})
-*/
 var SeoPreview = {};
 SeoPreview ={
 	find: function(){
 		seo_object = {};
 		seo_object = {
-			h1 : $("h1").text() || false,
 			title : $("title").text() || false,
+			h1 : $("h1").text() || false,
+			h2 : $("h2").text() || false,
 			description : $("meta[name=description]").attr("content") || false,
 			keywords : $("meta[name=keywords]").attr("content") || false,
 			url : document.URL || false,
@@ -34,8 +13,8 @@ SeoPreview ={
 			og_type : $('meta[property="og:type"]').attr("content") || false,
 			og_site_name : $('meta[property="og:site_name"]').attr("content") || false,
 			og_title : $('meta[property="og:title"]').attr("content") || false,
-			og_image : $('meta[property="og:image"]').attr("content") || false,
-			og_description : $('meta[property="og:description"]').attr("content") || false
+			og_description : $('meta[property="og:description"]').attr("content") || false,
+			og_image : $('meta[property="og:image"]').attr("content") || false
 		};
 		return seo_object;
 	}
--- show.js
+++ show.js
@@ -14,7 +14,7 @@ $(function(){
 		var td = $('<td>').text(seo_object[seo_tag]);
 
 		if (seo_tag ==="og_image") {
-			var image = $("<image>").attr("src", seo_object[seo_tag]);
+			var image = $("<image>").attr("src", seo_object[seo_tag]).css({width: "116px", height: "116px"});
 			td = $("<td>").append(image);
 		}
 		if (seo_object[seo_tag] == false){
@@ -24,35 +24,4 @@ $(function(){
 	};
 	div.append(table.append(tbody));
 	$("body").append(div);
-})
-
-/*
-function setView(response){	
- 	seo_object = response;
-	// 閉じる
-    $("body").on("click", "#seo_preview_close",function(e){
-		e.preventDefault();
-		$("#seo_preview_div").hide();
-	});
-
-    // DOMをセットする
-	var div = $("<div>").attr({id: "seo_preview_div"})
-	var table = $("<table>").css({width: "500px"}).addClass("table");
-	var tbody = $("<tbody>")
-	var close_mark = $("<a>").attr({href: "#", id: "seo_preview_close"}).text("[ x ]").css({})
-	for (var seo_tag in seo_object) {
-		var tr = $('<tr>')
-		var th = $("<th>").text(seo_tag);
-		var td = $('<td>').text(seo_object[seo_tag]);
-
-		if (seo_tag ==="og_image") {
-			var image = $("<image>").attr("src", seo_object[seo_tag]);
-			td = $("<td>").append(image)
-		}
-		tbody.append(tr.append(th).append(td))
-	};
-	div.append(close_mark).append(table.append(tbody));
-	$("body").append(div)
-}
-*/
-//
+});
